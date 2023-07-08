467022171b8c440cc27fd5f93230ca826b71bfb1
mahboubeh shoubi <mahboobeh.shobi.65@gmail.com>
2023-07-08 20:02:49 +0330

    Recponsive HeaderPage.js by React-Bootstrap

--- src/components/HeaderPages.js
+++ src/components/HeaderPages.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 //CSS
 import Styles from "../css/HeaderPages.module.css";
 
 //Imags
-import  Logo from "../image/logo/khadamat-logo.png";
+import Logo from "../image/logo/khadamat-logo.png";
+import Logo2 from "../image/logo/logo-khadamat.png";
 
 //Icons
 import FindUs from "../assets/icons/map.svg";
@@ -24,102 +30,114 @@ const HeaderPages = (props) => {
         <div>
 
             <header>
-                <section className={Styles.header}>
+                <Container fluid>
+                    <Row className={Styles.header}>
+                        <Col md={2} lg={2} className={Styles.logo}>
+                            <a href="../../index.html">
+                                <img src={Logo} /></a>
+                        </Col>
 
-                    <section className={Styles.logo}>
-                        <a href="../../index.html">
-                            <img src={Logo} /></a>
-                    </section>
+                        <Col xs={9} sm={9} md={8} lg={8}>
+                            <Row className={Styles.communication}>
 
-                    <section className={Styles.communication}>
+                                <Col>
+                                    <section className={Styles.item1}>
+                                        <section>
+                                            <img src={FindUs} className={Styles.icofontMap} />
+                                        </section>
+                                        <section className={Styles.itemRightText}>
+                                            <span>Find Us</span>
+                                            <span>325 admirat unit,North cost,USA</span>
+                                        </section>
+                                    </section>
+                                </Col>
 
-                        <section className={Styles.communicationItems}>
-                            <section className={Styles.item1}>
-                                <section className={Styles.itemLeftIcon}>
-                                    <img src={FindUs} className={Styles.icofontMap} />
-                                </section>
-                                <section className={Styles.itemRightText}>
-                                    <span>Find Us</span>
-                                    <span>325 admirat unit, North cost, USA</span>
-                                </section>
-                            </section>
-                        </section>
+                                <Col>
+                                    <section className={Styles.item2}>
+                                        <section>
+                                            <img src={Email} className={Styles.icofontEmail} />
+                                        </section>
+                                        <section className={Styles.itemRightText}>
+                                            <span>Email</span>
+                                            <span>admin@bbbbbbbb.com</span>
+                                        </section>
+                                    </section>
+                                </Col>
 
-                        <section className={Styles.communicationItems}>
-                            <section className={Styles.item2}>
-                                <section className={Styles.itemLeftIcon}>
-                                    <img src={Email} className={Styles.icofontEmail} />
-                                </section>
-                                <section className={Styles.itemRightText}>
-                                    <span>Email</span>
-                                    <span>admin@bbbbbbbb.com</span>
-                                </section>
-                            </section>
-                        </section>
+                                <Col>
+                                    <section className={Styles.item3}>
+                                        <section>
+                                            <img src={Call} className={Styles.icofontUiCall} />
+                                        </section>
+                                        <section className={Styles.itemRightText}>
+                                            <span>call us now</span>
+                                            <span>0123456789</span>
+                                        </section>
+                                    </section>
+                                </Col>
+                            </Row>
+                        </Col>
 
-                        <section className={Styles.communicationItems}>
-                            <section className={Styles.item3}>
-                                <section className={Styles.itemLeftIcon}>
-                                    <img src={Call} className={Styles.icofontUiCall} />
-                                </section>
-                                <section className={Styles.itemRightText}>
-                                    <span>call us now</span>
-                                    <span>0123456789</span>
-                                </section>
-                            </section>
-                        </section>
+                        <Col xs={3} sm={3} md={2} lg={2} className={Styles.getQuote}>
+                            <a href="#">Get A Quote</a>
+                        </Col>
 
-                    </section>
+                    </Row>
+                </Container>
 
-                    <section className={Styles.getQuote}>
-                        <a href="#">Get A Quote</a>
-                    </section>
-                </section>
 
                 {/* ----------------------------- Navbar----------------------------- */}
 
-                <nav>
-                    <section className={Styles.navbar}>
-                        <section className={Styles.navLeft}>
-                            <ul>
-                                <li><Link to="/">Home</Link></li>
-                                <li><Link to="/about">About</Link></li>
-                                <li><a href="#">Services</a></li>
-                                <li><Link to="/project">Works</Link></li>
-                                <li><Link to="/products">Shop</Link></li>
-                                <li><a href="#">Pages</a></li>
-                                <li><Link to="/contact">Contact</Link></li>
-                            </ul>
-                        </section>
+                <Navbar expand="md">
+                    <Container className={Styles.navbar}>
+                        <Navbar.Brand  className={Styles.logo2}>
+                            <Link to='/'>
+                                <img src={Logo2}/>
+                            </Link>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" className={Styles.hamburgerMenu}/>
+                        <Navbar.Collapse id="basic-navbar-nav" className={Styles.navCollapse}>
+                            <section className={Styles.navLeft}>
+                                <ul>
+                                    <li><Link to="/">Home</Link></li>
+                                    <li><Link to="/about">About</Link></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><Link to="/project">Works</Link></li>
+                                    <li><Link to="/products">Shop</Link></li>
+                                    <li><a href="#">Pages</a></li>
+                                    <li><Link to="/contact">Contact</Link></li>
+                                </ul>
+                            </section>
 
-                        <section className={Styles.navRight}>
-                            <ul>
-                                <li><a href="#"><img src={FaceBook} /></a></li>
-                                <li><a href="#"><img src={twitter} /></a></li>
-                                <li><a href="#"><img src={GooglePlus} /></a></li>
-                                <li><a href="#"><img src={Youtube} /></a></li>
-                                <li><a href="#"><img src={Search} /></a></li>
-                            </ul>
-                        </section>
 
-                    </section>
-                </nav>
+                            <section className={Styles.navRight}>
+                                <ul>
+                                    <li><a href="#"><img src={FaceBook} /></a></li>
+                                    <li><a href="#"><img src={twitter} /></a></li>
+                                    <li><a href="#"><img src={GooglePlus} /></a></li>
+                                    <li><a href="#"><img src={Youtube} /></a></li>
+                                    <li><a href="#"><img src={Search} /></a></li>
+                                </ul>
+                            </section>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
 
                 {/* <!---------------------  Main Header ------------------> */}
 
-                <section className={Styles.headerMain}>
-                    <section className={Styles.headerLeft}>
+                <Row className={Styles.headerMain}>
+                    <Col xs={12} sm={12} md={10} lg={10} className={Styles.headerLeft}>
                         <span> {part1} </span>
                         <span> {part2} </span>
-                    </section>
-                    <section className={Styles.headerRight}>
+                    </Col>
+                    <Col xs={12} sm={12} md={2} lg={2} className={Styles.headerRight}>
                         <img src={Home} alt="HomeIcon"/>
                         <Link to="/main">Home</Link>
                         <span> / </span>
                         <a href="#" className={Styles.rightLink}> {part3} </a>
-                    </section>
+                    </Col>
 
-                </section>
+                </Row>
             </header>
 
         </div>
